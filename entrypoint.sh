d05601029aeb48668cb6c6c507e71dfd4c31e77f
zhhailon <653602+zhhailon@users.noreply.github.com>
2021-03-14 21:17:28 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -l
 
-mv tests/Makefile ./
-mv tests/test_driver.cpp ./
+FILE=tests/prerun.sh
+if [ -f "$FILE" ]; then
+  cd tests
+  sh tests/prerun.sh
+  cd ..
+else 
+  mv tests/Makefile ./
+  mv tests/test_driver.cpp ./
+fi
 python3 /autograding.py
 res=$?
 echo ::set-output name=points::"$(cat points)"
